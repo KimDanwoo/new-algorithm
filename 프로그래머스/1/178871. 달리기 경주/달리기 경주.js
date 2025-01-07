5d905da7f9f444e1a618edd88e954e93ab500be6
Danwoo <84695769+KimDanwoo@users.noreply.github.com>
2025-01-08 07:18:05 +0900

    [level 1] Title: 달리기 경주, Time: 142.51 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,23 @@
+function solution(players, callings) {
+  let rankMap = {}
+  
+  players.forEach((player, i) => {
+    rankMap[player] = i
+  })
+
+  callings.forEach((call) => {
+    let currentRank = rankMap[call]
+    
+    if (currentRank > 0) {
+      let prevPlayer = players[currentRank - 1]
+
+      players[currentRank - 1] = call
+      players[currentRank] = prevPlayer
+
+      rankMap[call]--
+      rankMap[prevPlayer]++
+    }
+  })
+
+  return players
+}
