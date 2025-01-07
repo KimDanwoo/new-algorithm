22315b1ef27baea420620623166ec9a0881ef065
Danwoo <84695769+KimDanwoo@users.noreply.github.com>
2025-01-07 20:06:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.73 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,33 @@
+function solution(maps) {
+  const dx = [0, 1, 0, -1]
+  const dy = [1, 0, -1, 0]
+  const n = maps.length
+  const m = maps[0].length
+  let visited = Array.from(Array(n), () => Array(m).fill(0))
+
+  let queue = [[0, 0]]
+  visited[0][0] = 1
+
+  while (queue.length !== 0) {
+    let [x, y] = queue.shift()
+
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i]
+      let ny = y + dy[i]
+
+      if (
+        nx >= 0 &&
+        nx < n &&
+        ny >= 0 &&
+        ny < m &&
+        maps[nx][ny] === 1 &&
+        visited[nx][ny] === 0
+      ) {
+        visited[nx][ny] = visited[x][y] + 1
+        queue.push([nx, ny])
+      }
+    }
+  }
+
+  return visited[n - 1][m - 1] === 0 ? -1 : visited[n - 1][m - 1]
+}
